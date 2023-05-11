8e33d2a2c6a2edebc7fb345c9a3b485b25d5c1fe
Mohieddine Farid <farid.mohieddine3@gmail.com>
2023-05-11 22:31:50 +0100

    Finish login form layout

--- views/login.php
+++ views/login.php
@@ -1,10 +1,11 @@
 <?php $title = "Login"; ?>
 <?php include_once('../includes/header.php') ?>
 <link rel="stylesheet" href="../styles/main.css">
+<link rel="stylesheet" href="../styles/login.css">
 </head>
 
 <body class="font-inter text-white bg-gray-700 min-h-screen">
-  <header id="nav_bar">
+  <header id="nav_bar" class="m-6">
     <div class="flex container mx-auto items-center justify-between m-3">
       <div class="flex space-x-12">
         <a href="contact.php" class="hover:cursor-pointer hover:text-gray-300">Contact</a>
@@ -16,15 +17,15 @@
     </div>
   </header>
 
-  <section id="login" class="container mx-auto h-full flex flex-col items-center justify-center">
-    <h1 class="uppercase p-4 text-center text-6xl">Sign In</h1>
+  <section id="login" class="container mx-auto h-full flex flex-col items-center justify-center gap-6">
+    <h1 class="uppercase p-4 text-center text-3xl mt-28">Sign In</h1>
     <form action="../db/login.php" method="get" class="w-full max-w-md flex flex-col items-center justify-center gap-4">
-      <input type="text" name="name" id="name" placeholder="Name" class="placeholder:italic placeholder-white bg-transparent border-b-2 border-white focus:border-none focus:border-transparent">
-      <input type="password" name="password" id="password" placeholder="Password" class="placeholder-italic placeholder-white bg-transparent border-b-2 border-white focus:border-none focus:border-transparent">
-      <button type="submit" class="w-1/2 uppercase bg-green-500 text-white font-bold border-transparent p-1.5 hover:bg-green-600 hover:text-gray-200">Sign In</button>
-      <div class="flex items-center justify-between">
-        <p class="text-sm"><input type="checkbox" name="remember" id="remember"> Remember Me</p>
-        <a href="" class="text-sm">Forgot Password</a>
+      <input type="text" name="name" id="name" placeholder="Name" class="placeholder:italic placeholder-white bg-transparent border-b-2 font-3xl pr-28 py-2 border-white text-md focus:outline-none focus:border-b-2">
+      <input type="password" name="password" id="password" placeholder="Password" class="placeholder-italic placeholder-white bg-transparent border-b-2 font-3xl pr-28 py-2 border-white text-md focus:border-b-2 focus:outline-none">
+      <button type="submit" name="login" id="login" class="w-1/2 uppercase rounded-sm bg-green-500 text-white font-bold font-lg p-1.5 px-3 border-transparent hover:bg-green-600 hover:text-gray-200">Sign In</button>
+      <div class="flex items-center justify-between w-1/2">
+        <a href="" class="text-sm hover:cursor-pointer hover:text-gray-500">Sign Up</a>
+        <a href="" class="text-sm hover:cursor-pointer hover:text-gray-500">Forgot Password</a>
       </div>
     </form>
   </section>
