fa395c0bfb2257685fe3b243526bf26162d0872c
MohiZzine <farid.mohieddine3@gmail.com>
2023-06-10 17:59:14 +0100

    HHH

--- views/devis.php
+++ views/devis.php
@@ -3,11 +3,21 @@ if (!isset($_SESSION['user_id'])) {
   header('Location: views/login.php');
   exit();
 }
-
-include_once '../classes/database.class.php';
-
+require_once("../utils/database.php");
+// require_once '../classes/database.class.php';
 ?>
-
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>SNS Emplois Libres</title>
+  <!-- <link rel="stylesheet" href="styles/main.css"> -->
+  <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
+  <link rel="stylesheet" href="../styles/dashboard.css" />
+  <link rel="stylesheet" href="../styles/cards.css" />
+</head>
 <body>
   <!--  Body Wrapper -->
   <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
@@ -46,4 +56,4 @@ include_once '../classes/database.class.php';
   <script src="assets/js/app.min.js"></script>
   <script src="assets/libs/simplebar/dist/simplebar.js"></script>
 
-  <?php include 'views/footer.php'; ?>
+  <?php include '../includes/footer.php'; ?>
